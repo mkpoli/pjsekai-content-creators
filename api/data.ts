2a00c5e542eeafa16519d980a4d36d85fc21aaac
mkpoli <snowwolf1001@gmail.com>
2021-08-22 23:22:19 +0900

    Add googleapis

--- api/data.ts
+++ api/data.ts
@@ -1,7 +1,28 @@
-module.exports = (req, res) => {
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-  });
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { google } from 'googleapis'
+const sheets = google.sheets('v4');
+const auth = new google.auth.GoogleAuth({
+    credentials: {
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
+    },
+    scopes: [
+        'https://www.googleapis.com/auth/spreadsheets'
+    ],
+    projectId: process.env.GOOGLE_PROJECT_ID
+})
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  const { name = 'World' } = request.query;
+
+  const authClient = await auth.getClient()
+  google.options({
+    auth: authClient,
+  })
+
+  const res = await sheets.spreadsheets.get({
+    spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID,
+  })
+
+  response.status(200).send(`Hello ${name}! ${res}`);
 };
