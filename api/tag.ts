32ddcef96d6236c68b8cd42e389187f10895b177
mkpoli <snowwolf1001@gmail.com>
2022-01-31 02:38:34 +0900

    use JSON parse

--- api/tag.ts
+++ api/tag.ts
@@ -1,10 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { client, q, faunadb } from './fauna'
 export default async (request: VercelRequest, response: VercelResponse) => {
+  const { data: { name, id } } = JSON.parse(request.body)
   if (request.method === 'POST') {
     try {
       await client.query(
-        q.Call(q.Function('AddTagToChannel'), [request.body.data.name, request.body.data.id])
+        q.Call(q.Function('AddTagToChannel'), [name, id])
       )
       response.status(200).end()
     } catch (error) {
